cba18f7c5183e971bd514889b9267d30f766478f
pro-munachi <madustanley1@gmail.com>
2021-09-05 09:30:46 +0100

    'add'

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(cors())
 
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server has started in port: ${PORT}`));
 
