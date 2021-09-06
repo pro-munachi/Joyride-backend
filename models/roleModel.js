d5af6b819e85a1e2dd37ef5db82b44343471c4e6
pro-munachi <madustanley1@gmail.com>
2021-09-06 09:09:54 +0100

    'muna'

--- models/roleModel.js
+++ models/roleModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const roleSchema = new mongoose.Schema({
-    name: {type: String, required: true, unique: true},
+  name: { type: String, required: true, unique: true },
 })
 
 module.exports = Role = mongoose.model('role', roleSchema)
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -4,142 +4,140 @@ const jwt = require('jsonwebtoken')
 const User = require('../models/userModel')
 const Role = require('../models/roleModel')
 
-
- //fetch users
-
- router.get('/getUsers', async (req, res) => {
-     const allUsers = await User.find({})
-     res.json(allUsers)
- })
-
-
 //Register users
 
 router.post('/register', async (req, res) => {
-try {
-   let {email, password, passwordCheck, displayName, roles} = req.body
-   
-  // validate
+  try {
+    let { email, password, passwordCheck, displayName, roles } = req.body
 
-   if(!email || !password || !passwordCheck) {
-       res.status(400).json({msg: 'Not all fields have been filled'})
-   }
+    // validate
 
-   if(password.length < 5 ) {
-    res.status(400).json({msg: 'Password should be 5 characters and above '})
+    if (!email || !password || !passwordCheck) {
+      res.status(400).json({ msg: 'Not all fields have been filled' })
     }
 
-    if(password !== passwordCheck) {
-        res.status(400).json({msg: 'Enter the same password twice '})
-        }
-
-    const existingUser = await User.findOne({email: email})
-    if(existingUser) {
-        res.status(400).json({msg: 'An account with this user already exist '})
+    if (password.length < 5) {
+      res
+        .status(400)
+        .json({ msg: 'Password should be 5 characters and above ' })
     }
 
-    if(!displayName) {
-        displayName = email
+    if (password !== passwordCheck) {
+      res.status(400).json({ msg: 'Enter the same password twice ' })
+    }
+
+    const existingUser = await User.findOne({ email: email })
+    if (existingUser) {
+      res.status(400).json({ msg: 'An account with this user already exist ' })
+    }
+
+    if (!displayName) {
+      displayName = email
     }
 
     const salt = await bcrypt.genSalt()
     const passwordHash = await bcrypt.hash(password, salt)
-    
 
     const newUser = new User({
-        email,
-        password: passwordHash,
-        displayName,
-        roles
+      email,
+      password: passwordHash,
+      displayName,
+      roles,
     })
     const savedUser = await newUser.save()
     res.json(savedUser)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
 
+// Login users
+
+router.post('/login', async (req, res) => {
+  try {
+    const { email, password } = req.body
+
+    // validate
+
+    if (!email || !password) {
+      res.status(400).json({ msg: 'An account with this user already exist ' })
     }
 
-catch(err) {
-    res.status(500).json({error: err.message})}
+    const user = await User.findOne({ email: email })
+    if (!user) {
+      res.status(400).json({ msg: 'invalid email ' })
     }
- )
 
- // Login users
-
- router.post('/login', async (req, res) => {
-    try {
-        const {email, password} = req.body
-
-        // validate
-
-        if(!email || !password)  {
-            res.status(400).json({msg: 'An account with this user already exist '})
-        }
-
-        const user = await User.findOne({ email: email})
-        if(!user)  {
-            res.status(400).json({msg: 'invalid email '})
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password)
-        if(!isMatch)  {
-            res.status(400).json({msg: 'invalid credentials'})
-        }
-
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
-        res.json({
-            token,
-            user: {
-                id: user._id,
-                displayName: user.displayName,
-                email: user.email,
-                hasError: false,
-                message: 'login successful'
-            }
-        })
-    
-    
-
-    }catch(err) {
-    res.status(500).json({error: err.message})
-}
- })
-
- // Post roles
-
- router.post('/roles', async (req, res) => {
-    try {
-        const { name } = req.body
-
-        // validate roles as not to post the same role twice
-
-        const existingRole = await User.findOne({name: name})
-        if(existingRole) {
-            res.status(400).json({msg: 'Role already exist '})
-        }
-
-        const newRole = new Role({
-            name,
-            hasError: false,
-            message: 'role posted'
-        })
-        const savedRole = await newRole.save()
-        res.json(savedRole)
-
-    } catch(err) {
-        res.status(500).json({error: err.message})
+    const isMatch = await bcrypt.compare(password, user.password)
+    if (!isMatch) {
+      res.status(400).json({ msg: 'invalid credentials' })
     }
- })
 
- //fetch roles
+    const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET)
+    res.json({
+      token,
+      user: {
+        id: user._id,
+        displayName: user.displayName,
+        email: user.email,
+        hasError: false,
+        message: 'login successful',
+      },
+    })
+  } catch (err) {
+    res.status(500).json({ error: err.message, hasError: true })
+  }
+})
 
- router.get('/getRoles', async (req, res) => {
-     const allRoles = await Role.find({})
-     res.json(allRoles)
- })
+//fetch users
 
- 
+router.get('/', async (req, res) => {
+  const allUsers = await User.find({})
+  res.json(allUsers)
+})
 
+// get user by id
 
-    
+router.get('/:userId', async (req, res) => {
+  const user = await User.findById(req.params.userId)
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error('User not found')
+  }
+})
 
+// Post roles
+
+router.post('/roles', async (req, res) => {
+  try {
+    const { name } = req.body
+
+    // validate roles as not to post the same role twice
+
+    const existingRole = await User.findOne({ name: name })
+    if (existingRole) {
+      res.status(400).json({ msg: 'Role already exist ' })
+    }
+
+    const newRole = new Role({
+      name,
+      hasError: false,
+      message: 'role posted',
+    })
+    const savedRole = await newRole.save()
+    res.json(savedRole)
+  } catch (err) {
+    res.status(500).json({ error: err.message })
+  }
+})
+
+//fetch roles
+
+router.get('/getRoles', async (req, res) => {
+  const allRoles = await Role.find({})
+  res.json(allRoles)
+})
 
 module.exports = router
