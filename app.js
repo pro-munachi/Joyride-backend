6dc100de77e3055cf3a58daa0c09b7f73f6d5d61
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-09-11 21:49:05 +0100

    'muna'

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const cors = require('cors')
-require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
 
 //setup express
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server has started in port: ${PORT}`))
+app.listen(PORT, () => console.log(`Server has started in port: ${PORT}`));
 
 // setup mongoose
 
@@ -20,11 +24,11 @@ mongoose.connect(
   process.env.MONGODB_CONNECTION_STRING,
   { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
   (err) => {
-    if (err) throw err
-    console.log('Mongodb connection successful')
+    if (err) throw err;
+    console.log('Mongodb connection successful');
   }
-)
+);
 
 // setup routes
 
-app.use('/users', cors(), require('./routes/userRouter'))
+app.use('/users', require('./routes/userRouter'));
