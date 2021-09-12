a33705d0350f0f75c217507850f4229a83a9de7f
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-09-13 00:30:33 +0100

    una'

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const bcrypt = require('bcryptjs');
-const asyncHandler = require('express-async-handler');
+const router = require('express').Router()
+const bcrypt = require('bcryptjs')
+const asyncHandler = require('express-async-handler')
 
-const generateToken = require('../utils/generatetoken');
-const User = require('../models/userModel');
-const Role = require('../models/roleModel');
+const generateToken = require('../utils/generatetoken')
+const User = require('../models/userModel')
+const Role = require('../models/roleModel')
 
 //Register users
 
@@ -12,34 +12,34 @@ router.post(
   '/register',
   asyncHandler(async (req, res) => {
     let { email, password, passwordCheck, displayName, roles, profilePic } =
-      req.body;
+      req.body
 
     if (!email || !password || !passwordCheck) {
-      res.status(400).json({ msg: 'Not all fields have been filled' });
+      res.status(400).json({ msg: 'Not all fields have been filled' })
     }
 
     if (password.length < 5) {
       res
         .status(400)
-        .json({ msg: 'Password should be 5 characters and above ' });
+        .json({ msg: 'Password should be 5 characters and above ' })
     }
 
     if (password !== passwordCheck) {
-      res.status(400).json({ msg: 'Enter the same password twice ' });
+      res.status(400).json({ msg: 'Enter the same password twice ' })
     }
 
     if (!displayName) {
-      displayName = email;
+      displayName = email
     }
 
-    const salt = await bcrypt.genSalt();
-    const passwordHash = await bcrypt.hash(password, salt);
+    const salt = await bcrypt.genSalt()
+    const passwordHash = await bcrypt.hash(password, salt)
 
-    const userExists = await User.findOne({ email });
+    const userExists = await User.findOne({ email })
 
     if (userExists) {
-      res.status(400);
-      throw new Error('User already exists');
+      res.status(400)
+      throw new Error('User already exists')
     }
 
     const user = await User.create({
@@ -48,8 +48,7 @@ router.post(
       email,
       password,
       roles,
-      passwordCheck,
-    });
+    })
 
     if (user) {
       res.status(201).json({
@@ -59,22 +58,23 @@ router.post(
         roles: user.roles,
         token: generateToken(user._id),
         hasError: false,
-      });
+        profilePic: user.profilePic,
+      })
     } else {
-      res.status(400);
-      throw new Error('Invalid user data');
+      res.status(400)
+      throw new Error('Invalid user data')
     }
   })
-);
+)
 
 // Login
 
 router.post(
   '/login',
   asyncHandler(async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email })
 
     if (user && (await user.matchPassword(password))) {
       res.json({
@@ -84,68 +84,69 @@ router.post(
         roles: user.roles,
         token: generateToken(user._id),
         hasError: false,
-      });
+        profilePic: user.profilePic,
+      })
     } else {
       res.json({
         error: 'Invalid email or password',
         hasError: true,
-      });
+      })
     }
   })
-);
+)
 
 //fetch users
 
 router.get(
   '/',
   asyncHandler(async (req, res) => {
-    const allUsers = await User.find({});
-    res.json(allUsers);
+    const allUsers = await User.find({})
+    res.json(allUsers)
   })
-);
+)
 
 // get user by id
 
 router.get('/:userId', async (req, res) => {
-  const user = await User.findById(req.params.userId);
+  const user = await User.findById(req.params.userId)
   if (user) {
-    res.json(user);
+    res.json(user)
   } else {
-    res.status(404);
-    throw new Error('User not found');
+    res.status(404)
+    throw new Error('User not found')
   }
-});
+})
 
 // Post roles
 
 router.post('/roles', async (req, res) => {
   try {
-    const { name } = req.body;
+    const { name } = req.body
 
     // validate roles as not to post the same role twice
 
-    const existingRole = await User.findOne({ name: name });
+    const existingRole = await User.findOne({ name: name })
     if (existingRole) {
-      res.status(400).json({ msg: 'Role already exist ' });
+      res.status(400).json({ msg: 'Role already exist ' })
     }
 
     const newRole = new Role({
       name,
       hasError: false,
       message: 'role posted',
-    });
-    const savedRole = await newRole.save();
-    res.json(savedRole);
+    })
+    const savedRole = await newRole.save()
+    res.json(savedRole)
   } catch (err) {
-    res.status(500).json({ error: err.message });
+    res.status(500).json({ error: err.message })
   }
-});
+})
 
 //fetch roles
 
 router.get('/getRoles', async (req, res) => {
-  const allRoles = await Role.find({});
-  res.json(allRoles);
-});
+  const allRoles = await Role.find({})
+  res.json(allRoles)
+})
 
-module.exports = router;
+module.exports = router
