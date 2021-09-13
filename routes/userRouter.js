92aecc8097c31e65559bc83afecf8a6ecaa2de83
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-09-13 19:54:51 +0100

    'muna'

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -228,7 +228,7 @@ router.post(
                                                   password has been generated for you. To reset your password, click the
                                                   following link and follow the instructions.
                                               </p>
-                                              <a href="http://localhost:3000auth/reset-password/${token}"
+                                              <a href="http://localhost:3000/auth/reset-password/${token}"
                                                   style="background:#20e277;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">Reset
                                                   Password</a>
                                           </td>
