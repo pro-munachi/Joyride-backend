8b4bd3a7cb16f0137ccac2d8115906c7f72fc293
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-09-16 10:11:18 +0100

    'added-changes'

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,34 +5,29 @@ const User = require('../models/userModel')
 const protect = asyncHandler(async (req, res, next) => {
   let token
 
-  console.log(req.headers)
+  console.log(req.header)
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     try {
       token = req.headers.authorization.split(' ')[1]
-      console.log(token)
 
-      if (token) {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
-        console.log(decoded)
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        req.user = await User.findById(decoded.id)
+      req.user = await User.findById(decoded.id)
 
-        next()
-      } else {
-        res.json({
-          hasError: 'Not authorized, no token',
-        })
-        console.log('ewo')
-      }
+      next()
     } catch (error) {
       console.error(error)
-      res.json({
-        hasError: 'Not authorized, token failed',
-      })
+      res.status(401)
+      throw new Error('Not authorized, token failed')
     }
+  }
+
+  if (!token) {
+    res.status(401)
+    throw new Error('Not authorized, no token')
   }
 })
 
