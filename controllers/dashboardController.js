6513784d8d8819b165361552e38563815c665588
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-10-27 15:10:48 +0100

    total order price

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -2,6 +2,74 @@ const asyncHandler = require('express-async-handler')
 const moment = require('moment')
 const Order = require('../models/orderModel')
 
+// Get total price
+// GET /dashboard/today
+
+const totalPrice = asyncHandler(async (req, res) => {
+  let orders = await Order.find({
+    user: req.user._id,
+  })
+
+  if (orders) {
+    // Total Order
+
+    let total = orders.reduce(function (acc, curr) {
+      return acc + curr.totalPrice
+    }, 0)
+
+    // Yearly total order
+
+    let test = []
+
+    for (let i = 0; i < orders.length; i++) {
+      const d = new Date()
+      const year = d.getFullYear()
+      let startYear = `${year}0101`
+      let endYear = `${year}1231`
+
+      if (orders[i].date >= startYear && orders[i].date <= endYear) {
+        test.push(orders[i])
+      }
+    }
+    let totalYear = test.reduce(function (acc, curr) {
+      return acc + curr.totalPrice
+    }, 0)
+
+    // Monthly Total Orders
+
+    let mon = []
+
+    for (let i = 0; i < orders.length; i++) {
+      const date = new Date()
+      var firstDay = new Date(date.getFullYear(), date.getMonth(), 1)
+      var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0)
+
+      if (
+        orders[i].date >= moment(new Date(firstDay)).format('YYYYMMDD') &&
+        orders[i].date <= moment(new Date(lastDay)).format('YYYYMMDD')
+      ) {
+        mon.push(orders[i])
+      }
+    }
+    let totalMonth = mon.reduce(function (acc, curr) {
+      return acc + curr.totalPrice
+    }, 0)
+
+    res.json({
+      hasError: false,
+      totalYear,
+      totalYearLength: test.length,
+      totalMonth,
+      totalMonthLength: mon.length,
+      total,
+      totalLength: orders.length,
+    })
+  } else {
+    res.json({
+      hasError: true,
+    })
+  }
+})
 // Get todays order
 // GET /dashboard/today
 
@@ -10,7 +78,7 @@ const todaysOrder = asyncHandler(async (req, res) => {
     user: req.user._id,
     date: moment(new Date()).format('YYYYMMDD'),
   })
-  res.json(orders)
+  res.json({ orders, hasError: false })
 })
 
 // Get todays price
@@ -25,12 +93,10 @@ const todaysPrice = asyncHandler(async (req, res) => {
     let test = orders.reduce(function (acc, curr) {
       return acc + curr.totalPrice
     }, 0)
-
-    console.log(test)
     res.json({ test, hasError: false })
   } else {
     res.json({ hasError: true })
   }
 })
 
-module.exports = { todaysOrder, todaysPrice }
+module.exports = { todaysOrder, todaysPrice, totalPrice }
--- routes/dashboardRouter.js
+++ routes/dashboardRouter.js
@@ -2,9 +2,11 @@ const router = require('express').Router()
 const {
   todaysOrder,
   todaysPrice,
+  totalPrice,
 } = require('../controllers/dashboardController')
 const { protect, admin } = require('../middleware/authMiddleware')
 
+router.route('/totalprice').get(protect, totalPrice)
 router.route('/today').get(protect, todaysOrder)
 router.route('/today/price').get(protect, todaysPrice)
 
