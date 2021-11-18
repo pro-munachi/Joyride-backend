2e49addbc530f3fac266af3926273854d31fa557
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-11-18 09:32:23 +0100

    added parameter limit

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ require('dotenv').config()
 
 const app = express()
 
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(
+  bodyParser.urlencoded({
+    limit: '50mb',
+    extended: true,
+    parameterLimit: 50000,
+  })
+)
+
 app.use(express.json())
 app.use(
   cors({
