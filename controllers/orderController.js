6a8c6106fbae6ddc6dd179be60acf3f66c76b08c
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-11-22 21:10:11 +0100

    delivered-order

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -464,8 +464,10 @@ const deliverOrder = asyncHandler(async (req, res) => {
     isDelivered: true,
   })
 
+  console.log(order)
+
   if (order) {
-    const orde = await Order.findById(id)
+    const orde = await Order.findById(req.params.id)
 
     let use = orde.user
 
@@ -478,7 +480,6 @@ const deliverOrder = asyncHandler(async (req, res) => {
     res.json({
       hasError: false,
       message: 'order has been delivered',
-      order,
     })
   } else {
     res.json({
