1be632b9df034ebb3bcaea61fa9671163a436a61
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-11-23 11:54:11 +0100

    muna

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -11,18 +11,11 @@ const protect = asyncHandler(async (req, res, next) => {
     try {
       token = req.headers.authorization.split(' ')[1]
 
-      if (jwt.verify(token) === false) {
-        res.json({
-          hasError: true,
-          message: 'token has expired',
-        })
-      } else {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+      const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        req.user = await User.findById(decoded.id)
+      req.user = await User.findById(decoded.id)
 
-        next()
-      }
+      next()
     } catch (error) {
       res.status(401)
       throw new Error('Not authorized, token failed')
