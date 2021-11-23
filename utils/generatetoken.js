b30fea304006ff46b4da344c82685c5aee97d10e
Madu Stanley Munachiso <62253488+pro-munachi@users.noreply.github.com>
2021-11-23 11:49:09 +0100

    muna

--- utils/generatetoken.js
+++ utils/generatetoken.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken')
 
 const generateToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresInMinutes: 1,
+    expiresInMinutes: 10,
   })
 }
 
